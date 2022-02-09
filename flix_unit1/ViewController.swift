488fe61a73d97f03004fb3d67cb0ae3fbd75bb3b
Ashley Garcia <32247665+ashgarc20@users.noreply.github.com>
2022-02-08 22:16:05 -0800

    code for flix

---
+++ flix_unit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix_unit1
+//
+//  Created by Ashley Garcia-Arellano on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flix_unit1Tests/flix_unit1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  flix_unit1Tests.swift
+//  flix_unit1Tests
+//
+//  Created by Ashley Garcia-Arellano on 2/8/22.
+//
+
+import XCTest
+@testable import flix_unit1
+
+class flix_unit1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
